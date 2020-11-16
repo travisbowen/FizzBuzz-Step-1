8e36f767923e6e414b67adb284ab7d7dae032bf4
Travis Bowen <Travis.bowen@tee2.org>
2020-11-16 13:53:37 -0800

    Completed fizzbuzz unit test for function

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { fizzBuzz } from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("fizzbuzz function map over and check for all 4 possibilities", () => {
+	const testArray = fizzBuzz([1, 3, 5, 15]);
+	expect(testArray[0]).toBe(1);
+	expect(testArray[1]).toBe("fizz");
+	expect(testArray[2]).toBe("buzz");
+	expect(testArray[3]).toBe("fizzbuzz");
 });
